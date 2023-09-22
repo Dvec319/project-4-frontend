76327b54240d711edf9b33332d2967adea9e161d
mdelvecchio <mdelvecchio3119@gmail.com>
2023-09-22 13:25:13 -0400

    adding update action

--- src/actions.js
+++ src/actions.js
@@ -28,3 +28,32 @@ export const createAction = async ({request}) => {
     // redirect to index
     return redirect('/athletes/')
 }
+
+// Update Action
+export const updateAction = async ({request, params}) => {
+    // grabbing the id
+    const id = params.id
+    // parse the form data
+    const formData = await request.formData();
+    // construct updated athlete
+    const updatedAthlete = {
+        name: formData.get('name'),
+        age: formData.get('age'),
+        sport: formData.get('sport'),
+        team: formData.get('team'),
+        position: formData.get('position'),
+        year: formData.get('year')
+    }
+
+    // request to update athlete
+    await fetch (url + id, {
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedAthlete)
+    })
+
+    // redirect to show page
+    return redirect(`/athletes/post/${id}`)
+}
