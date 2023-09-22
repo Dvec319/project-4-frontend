61ffc0816d3a6db7ca58e636cd5892af899a4004
mdelvecchio <mdelvecchio3119@gmail.com>
2023-09-22 13:12:38 -0400

    show page up and running

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Post({post}) {
 	return <div>
-        <Link to={`/post/${post.id}`}>
+        <Link to={`/athletes/post/${post.id}`}>
             <h1>{post.name}</h1>
             <h2>{post.sport}</h2>
         </Link>
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,17 @@
+import { Link, useLoaderData } from "react-router-dom";
+
 function Show(props) {
-	return <div>Show</div>;
+    const athlete = useLoaderData()
+
+	return <div>
+        <h1>{athlete.name}</h1>
+        <h2>{athlete.age}</h2>
+        <h2>{athlete.sport}</h2>
+        <h2>{athlete.team}</h2>
+        <h2>{athlete.position}</h2>
+        <h2>{athlete.year}</h2>
+        <Link to='/athletes/'><button>Back Home</button></Link>
+    </div>;
 }
 
 export default Show;
